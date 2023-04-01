f65c791aa473ca29ee054a5fd1f7c321dcf4e4c8
Blan25ga <gabriel.bl@hotmail.com.ar>
2023-04-02 00:34:00 +1000

    Realizacion de tareas de clase3

--- tareas/clase-3/tarea-clase-3.js
+++ tareas/clase-3/tarea-clase-3.js
@@ -7,19 +7,39 @@
 // Imprimir "Hola " y el nombre, " te llamás igual que mi ..."
 // Si no, simplemente imprimir "Hola " + nombre!
 
-let nombre = prompt("¿Cuál es tu nombre ?")
-const miNombre = "Gabriel"
-let otroNombre = "Juan"
+const nombre = prompt("¿Cuál es tu nombre ?").toLocaleLowerCase();
+const miNombre = "gabriel";
+const otroNombre = "juan";
+
 if (nombre === miNombre) {
+    alert(`Hola, Tocayo! Yo también me llamo ${miNombre}.`);
     console.log(`Hola, Tocayo! Yo también me llamo ${miNombre}.`);
 } else if (nombre === otroNombre) {
+    alert(`Hola ${otroNombre}, te llamás igual que mi primo!!!`);
     console.log(`Hola ${otroNombre}, te llamás igual que mi primo!!!`);
 } else {
+    alert(`Hola ${nombre}!`);
     console.log(`Hola ${nombre}!`);
 }
+
 //Tarea 2:
 // Preguntar la edad del usuario
 // Hacerle saber si tiene más, menos ó la misma edad que nosotros.
+
+let edad_usuario = prompt("¿que edad tienes?");
+let miEdad = 36;
+
+if (edad_usuario > miEdad) {
+    alert(`Tienes ${edad_usuario} años, sos mayor que yo.`);
+    console.log(`Tienes ${edad_usuario} años, sos mayor que yo.`);
+} else if (edad_usuario < miEdad) {
+    alert(`Tienes ${edad_usuario} años, sos menor que yo.`);
+    console.log(`Tienes ${edad_usuario} años, sos menor que yo.`);
+} else {
+    alert(`Tienes ${edad_usuario} años, tenemos la misma edad!!!`);
+    console.log(`Tienes ${edad_usuario} años, tenemos la misma edad!!!`);
+}
+
 
 //Tarea 3:
 // Preguntarle al usuario si tiene documento, y que conteste con "si" o "no".
@@ -29,3 +49,22 @@ if (nombre === miNombre) {
 // Si no tiene documento, no dejarlo entrar al bar.
 // Si no entendemos la respuesta, le decimos que no entendimos la respuesta.
 // Punto bonus: SI, NO, Si, No, si, no.
+
+let tieneDocumento = prompt('¿Tienes documento ? (si / no)').toLowerCase();
+
+if (tieneDocumento === 'si') {
+    let edad = prompt('¿Cuál es tu edad ?');
+    if (edad >= 18) {
+        alert('Bienvenido, puedes pasar al bar!');
+        console.log('Bienvenido, puedes pasar al bar!');
+    } else {
+        alert('Lo siento, eres menor de edad!!!');
+        console.log('Lo siento, eres menor de edad!!!');
+    }
+} else if (tieneDocumento === 'no') {
+    alert('Lo siento, no puedes entrar sin documento.');
+    console.log('Lo siento, no puedes entrar sin documento.');
+} else {
+    alert('No entendí tu respuesta.');
+    console.log('No entendí tu respuesta.');
+}
